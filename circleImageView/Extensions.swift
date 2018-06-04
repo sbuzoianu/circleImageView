7159a0ff223d1aa8620c9431081486fff652fcd3
Stefan Buzoianu <stefan@buzoianu.ro>
2018-06-04 14:42:41 +0300

    rounded image

---
+++ circleImageView/Extensions.swift
@@ -0,0 +1,53 @@
+//
+//  Extensions.swift
+//  circleImageView
+//
+//  Created by Synergy on 04/06/2018.
+//  Copyright © 2018 Synergy.com.nl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    func fromBase64() -> UIImage{
+        let dataDecoded  = NSData(base64Encoded: self, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)!
+        let image = UIImage(data: dataDecoded as Data)
+        return image!
+    }
+    
+}
+
+extension UIImage{
+    
+    func resizeWith(width: CGFloat) -> UIImage? {
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = self
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        imageView.layer.render(in: context)
+        guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
+        UIGraphicsEndImageContext()
+        return result
+    }
+    
+    var circle: UIImage? {
+        let square = CGSize(width: min(size.width, size.height), height: min(size.width, size.height))
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: square))
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = self
+        imageView.layer.cornerRadius = square.width/2
+        imageView.layer.masksToBounds = true
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        imageView.layer.render(in: context)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
+    }
+    
+}
+
+
+
